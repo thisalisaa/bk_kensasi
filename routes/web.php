73037d316e7f71fbbd07eb7479c44e345ba2767d
ayusantika28 <ayusantika2802@gmail.com>
2023-12-31 17:29:53 +0700

    perubahan ayu

--- routes/web.php
+++ routes/web.php
@@ -66,17 +66,6 @@ Route::middleware(['auth'])->group(function () {
 });
 // Rute untuk data siswa
 Route::resource('datasiswa', DataSiswaController::class);
-<<<<<<< HEAD
-//Route::get('/datasiswa', [DataSiswaController::class, 'index']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
-=======
-// routes/web.php
-Route::delete('datasiswa/{datasiswa}', 'DataSiswaController@destroy')->name('datasiswa.destroy');
-
->>>>>>> 68a7de571d5fe9447e2369b4faff1cb40c8db3b5
 
 // Rute untuk login
 Route::middleware(['guest'])->group(function () {
@@ -124,9 +113,7 @@ Route::middleware(['web', 'guest'])->group(function () {
     Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 });
 
-<<<<<<< HEAD
-// Rute untuk home setelah login
-=======
+
 //route layanan dan keterangan lain
 Route::resource('/layanan', LayananMasalahController::class);
 
@@ -144,7 +131,6 @@ Route::get('/biodata', [BiodataController::class, 'biodata']);
 //route home
 Auth::routes();
 
->>>>>>> 68a7de571d5fe9447e2369b4faff1cb40c8db3b5
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Auth::routes();
 
